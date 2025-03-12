63aeac0cb3ffe0aac653f837c9142bc0e1a0081e
Keeeeeeeeeeeeeeeeeeen <114154295+Keeeeeeeeeeeeeeeeeeen@users.noreply.github.com>
2025-03-12 23:26:19 +0800

    g

--- firebase.js
+++ firebase.js
@@ -10,23 +10,13 @@
 // const db = admin.firestore();
 
 // module.exports = db;
-require("dotenv").config();
 const admin = require("firebase-admin");
 
-let serviceAccount;
-
-if (process.env.FIREBASE_CREDENTIALS) {
-  // Decode base64 from Railway env variable
-  serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_CREDENTIALS, "base64").toString("utf-8"));
-} else {
-  // Load from file when running locally
-  serviceAccount = require("./firebase_wordbridge.json");
-}
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_CREDENTIALS, "base64").toString("utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 module.exports = db;
