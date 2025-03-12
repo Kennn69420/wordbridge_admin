a0be88a2d05d727a28e51175b168d77bdd864262
Keeeeeeeeeeeeeeeeeeen <114154295+Keeeeeeeeeeeeeeeeeeen@users.noreply.github.com>
2025-03-12 23:00:46 +0800

    try

--- firebase.js
+++ firebase.js
@@ -1,12 +1,26 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./firebase_wordbridge.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// //   databaseURL: "https://your-project-id.firebaseio.com",
+// });
+
+// const db = admin.firestore();
+
+// module.exports = db;
+
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase_wordbridge.json");
+
+const credentials = process.env.FIREBASE_CREDENTIALS
+  ? JSON.parse(Buffer.from(process.env.FIREBASE_CREDENTIALS, "base64").toString("utf8"))
+  : require("./firebase_wordbridge.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://your-project-id.firebaseio.com",
+  credential: admin.credential.cert(credentials),
 });
 
 const db = admin.firestore();
-
-module.exports = db;
+module.exports = { admin, db };
