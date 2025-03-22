c984fd8ce12de61b3271f77bc71160969fa492fd
Keeeeeeeeeeeeeeeeeeen <114154295+Keeeeeeeeeeeeeeeeeeen@users.noreply.github.com>
2025-03-22 08:50:49 +0800

    added login

--- firebase.js
+++ firebase.js
@@ -1,30 +1,10 @@
-// require('dotenv').config()
-// const admin = require("firebase-admin");
-// const serviceAccount = require("./firebase_wordbridge.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// //   databaseURL: "https://your-project-id.firebaseio.com",
-// });
-
-// const db = admin.firestore();
-
-// module.exports = db;
-require("dotenv").config();
+require('dotenv').config()
 const admin = require("firebase-admin");
-
-let serviceAccount;
-
-if (process.env.FIREBASE_CREDENTIALS) {
-  // Decode base64 from Railway env variable
-  serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_CREDENTIALS, "base64").toString("utf-8"));
-} else {
-  // Load from file when running locally
-  serviceAccount = require("./firebase_wordbridge.json");
-}
+const serviceAccount = require("./firebase_wordbridge.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://your-project-id.firebaseio.com",
 });
 
 const db = admin.firestore();
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const adminRoutes = require("./routes/mainRoutes");
+const fs = require("fs");
+const json = require("./firebase_wordbridge.json");
+console.log(JSON.stringify(json));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
